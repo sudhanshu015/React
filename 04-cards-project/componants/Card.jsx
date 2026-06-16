30ab2de728595e506e56f773dafb1d9b11642c79
sudhanshu015 <sudhanshukumar1139@gmail.com>
2026-06-17 01:40:41 +0530

    today 17/06/26 update 1.4

---
+++ 04-cards-project/componants/Card.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import{ Bookmark} from 'lucide-react'
+
+const Card = () => {
+  return (
+        <div className="card">
+       <div> <div className="top">
+          <img src="https://imgs.search.brave.com/GxVOq9xuqtiB4Tpa8JJxeWKumoDP0A9x9UXm7yU0e4A/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdDMu/ZGVwb3NpdHBob3Rv/cy5jb20vMTAwMTg2/MC8xNjM3NS9pLzQ1/MC9kZXBvc2l0cGhv/dG9zXzE2Mzc1NzYz/Mi1zdG9jay1waG90/by1hbWF6b24tbG9n/by1vbi1hLXdoaXRl/LmpwZw" alt="" />
+          <button>Save<Bookmark size={12} /></button>
+        </div>
+        <div className="centre">
+          <h3>Amazon <span>5 days ago</span></h3>
+          <h2>Senior UI/UX Designer</h2>
+          <div className='tag'>
+            <h4>Part Time </h4>
+            <h4>SeniorLevel</h4>
+          </div>
+        </div></div>
+        <div className="bottom">         
+            <div>
+              <h3>$120/hr</h3>
+              <p> Mumbi,india</p>
+            </div>
+            <button>Apply Now</button>
+        </div>
+      </div>
+      
+  )
+}
+
+export default Card
---
+++ 04-cards-project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Card from '../componants/Card'
+const App = () => {
+  return (
+    <div className='parent'>
+      <Card/>
+    </div>
+  )
+}
+
+export default App
