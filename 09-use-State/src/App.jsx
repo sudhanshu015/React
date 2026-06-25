78893f18be095ead75766f0509d64fe0cf0ec00d
sudhanshu015 <sudhanshukumar1139@gmail.com>
2026-06-25 23:27:19 +0530

    today 25/6/26 update 2.3

---
+++ 09-use-State/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+
+  
+  return (
+    <div>
+      <h1>0</h1>
+      <button>increase</button>
+      <button>decrease</button>
+    </div>
+  )
+}
+
+export default App
