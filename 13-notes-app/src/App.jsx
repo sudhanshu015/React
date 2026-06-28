fd0b9f9d8f92a06afaa42017463ffd82316c5d9d
sudhanshu015 <sudhanshukumar1139@gmail.com>
2026-06-28 22:50:02 +0530

    today 28/06/26  update 2.6

---
+++ 13-notes-app/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <form  className=' flex  justify-between '>
+        <input type="text" 
+         placeholder='Enter Notes Heading '
+         />
+         <textarea name="" 
+         id="" 
+         placeholder='Enter deitails'>
+
+         </textarea>
+      </form>
+    </div>
+  )
+}
+
+export default App
